314879467eaf58e8507339ef9eaded6b4df5875e
fernando.silva@dellead.com <fernando.silva@dellead.com>
2021-05-07 18:11:42 -0300

    first commit

---
+++ CartaoViagemWeb/Models/Bilhete.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CartaoViagemWeb.Models
+{
+    public class Bilhete
+    {
+        public string Descricao { get; set; }
+        public DateTime Validade { get; set; }
+    }
+}
